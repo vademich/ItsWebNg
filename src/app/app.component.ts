7170b819e0a6388dcbc32e5fa297cd0cee069084
vademich <goncharov1999vadim@gmail.com>
2023-11-29 10:54:58 +0300

    truck + caption fully animated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', '../fonts/fonts.css']
 })
 export class AppComponent {
   title = 'ItsWebNg';
--- src/app/truck/truck.component.ts
+++ src/app/truck/truck.component.ts
@@ -8,16 +8,16 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
       state('truckStart',
       style({
         backgroundImage: 'url(../../../assets/images/truck.svg)',
-        'transform': 'translateX(750px)',
+        'transform': 'translateX(400px)',
         'opacity': '0',
       })),
       state('truckFinish',
       style({
         backgroundImage: 'url(../../../assets/images/truck.svg)',
         'opacity': 1,
-        'transform': 'translateX(500px)',
+        'transform': 'translateX(-100px)',
       })),
-      transition('truckStart => truckFinish', [animate('1.5s')])
+      transition('truckStart => truckFinish', [animate('1s ease-out')])
     ]),
     trigger('lightHover', [
       state('on',
@@ -32,6 +32,28 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
       })),
       transition('on => off', [animate('0.5s')]),
       transition('off => on', [animate('0.5s')])
+    ]),
+    trigger('captionHover', [
+      state('invisible',
+      style({
+        opacity: 0
+      })),
+      state('visible',
+      style({
+        opacity: 1
+      })),
+      transition('invisible => visible', [animate('1s')]),
+    ]),
+    trigger('lineHover', [
+      state('width0',
+      style({
+        width: '0px'
+      })),
+      state('width500',
+      style({
+        width: '500px'
+      })),
+      transition('width0 => width500', [animate('1s ease-in')]),
     ])
   ],
   templateUrl: './truck.component.html',
@@ -45,6 +67,14 @@ export class TruckComponent {
   isLightHover = false;
   lightOnOff(){
     this.isLightHover = !this.isLightHover;
+  }
+  isCaptionHover = false;
+  captionShow(){
+    this.isCaptionHover = true;
+  }
+  isLineHover = false;
+  lineShow(){
+    this.isLineHover = true;
   }
 
 }
