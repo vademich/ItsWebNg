fff1e8c3d03a1c52333918c6952e3bf79bfc90ff
vademich <goncharov1999vadim@gmail.com>
2023-11-29 15:10:39 +0300

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { TruckComponent } from './truck/truck.component';
 import { MainComponent } from './main/main.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
     imports: [BrowserModule, BrowserAnimationsModule],
-    declarations: [AppComponent, TruckComponent, MainComponent],
+    declarations: [AppComponent, TruckComponent, MainComponent, HeaderComponent],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
