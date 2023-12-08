79f47804d312cd0a0ff91c643919a211b9fda3c3
vademich <goncharov1999vadim@gmail.com>
2023-12-08 12:21:39 +0300

    header для моб версии

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-header',
+  animations: [
+    trigger('menuClick', [
+      state('menu-open',
+      style({
+        height: '812px',
+        opacity: 1
+      })),
+      state('menu-close',
+      style({
+        height: '56px'
+      })),
+      transition('menu-close => menu-open', [animate('0.5s ease-in')]),
+      transition('menu-open => menu-close', [animate('0.5s ease-in')]),
+      state('menu',
+      style({
+        'background-image': 'url(../../assets/images/menu.svg)'
+      })),
+      state('cross',
+      style({
+        'background-image': 'url(../../assets/images/cross.svg)'
+      })),
+      transition('menu => cross', [animate('0.1s ease-in')]),
+      transition('cross => menu', [animate('0.1s ease-in')])
+    ])
+  ],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-
+  clicked = false;
+  showMenu() {
+    this.clicked = !this.clicked;
+  }
 }
