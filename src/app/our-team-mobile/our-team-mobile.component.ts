4a5c4636bce6a045547412f63211bee4ecc469c1
vademich <goncharov1999vadim@gmail.com>
2023-12-20 11:28:16 +0300

    фотки сотрудников для моб версии

--- src/app/our-team-mobile/our-team-mobile.component.ts
+++ src/app/our-team-mobile/our-team-mobile.component.ts
@@ -44,70 +44,110 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
       })),
       transition('hide => show',[animate('0.5s ease-out')]),
       transition('show => hide',[animate('0.5s ease-in')])
-    ])
+    ]),
+    trigger('trigger-move-right', [
+      state('state-0',
+      style({
+        transform: 'translateX(0px)'
+      })),
+      state('state-1',
+      style({
+        transform: 'translateX(-427px)'
+      })),
+      state('state-2',
+      style({
+        transform: 'translateX(-854px)'
+      })),
+      state('state-3',
+      style({
+        transform: 'translateX(-1281px)'
+      })),
+      state('state-4',
+      style({
+        transform: 'translateX(-1708px)'
+      })),
+      transition('state-1 => state-0', [animate('0.5s ease-in-out')]),
+      transition('state-0 => state-1', [animate('0.5s ease-in-out')]),
+      transition('state-2 => state-1', [animate('0.5s ease-in-out')]),
+      transition('state-1 => state-2', [animate('0.5s ease-in-out')]),
+      transition('state-3 => state-2', [animate('0.5s ease-in-out')]),
+      transition('state-2 => state-3', [animate('0.5s ease-in-out')]),
+      transition('state-3 => state-4', [animate('0.5s ease-in-out')]),
+      transition('state-4 => state-3', [animate('0.5s ease-in-out')]),
+      transition('state-4 => state-0', [animate('0.5s ease-in-out')]),
+      transition('state-0 => state-4', [animate('0.5s ease-in-out')]),
+    ]),
   ],
   templateUrl: './our-team-mobile.component.html',
-  styleUrls: ['./our-team-mobile.component.css']//, '../css/typography.css']
+  styleUrls: ['./our-team-mobile.component.css']
 })
 export class OurTeamMobileComponent {
   text = `Сегодня люди - это самый важный и ценный ресурс. И с каждым днём их ценность будет расти всё больше и больше. Мы - команда предпринимателей нашей страны 🇷🇺 - покоряем новые рынки 🌎.
   `;
-  
-  image1 = '../assets/images/people/gerbert.png';
-  employeeName1 = 'Герберт Ромберг';
-  employeeJob1 = 'Основатель компании';
 
-  employeeName2 = 'Дмитрий Рыков';
-  employeeJob2 = 'Руководитель IT отдела';
+  images = ['../assets/images/people/01.png', '../assets/images/people/02.png', '../assets/images/people/03.png', '../assets/images/people/04.png', '../assets/images/people/05.png',];
+  employeeNames = ['Герберт Ромберг', 'Виктория Дементьевская', 'Оксана Костяева', 'Грануш Хачатрян', 'Денис Задорожный'];
+  employeeJobs = ['Основатель компании ITS Group, консалтинг',
+    'Руководитель Международного направления', 
+    'Директор IT-компании «ООО Айтишник», аккредитованной минцифрой',
+    'Генеральный директор компании «ООО М3», агрегатора автомобильного сервиса',
+    'Генеральный директор компании «ООО ПК», сети придорожных сервисов в России'];
 
-  employeeName3 = 'Вадим Гончаров';
-  employeeJob3 = 'Frontend developer';
-  image3 = '../assets/images/people/vadim.jpeg';
+  move = false;
+  move1 = false;
+  move2 = false;
+  move3 = false;
 
-  images = ['../assets/images/people/gerbert.png', '', '../assets/images/people/vadim.jpeg'];
-  employeeNames = ['Герберт Ромберг', 'Дмитрий Рыков', 'Вадим Гончаров'];
-  employeeJobs = ['Основатель компании', 'Руководитель IT отдела', 'Frontend developer'];
-
-  current_image = '../assets/images/people/gerbert.png';
-  current_name = 'Герберт Ромберг';
-  current_job = 'Основатель компании';
-  default_img = '../assets/images/people/background.png';
-
-  count_clicks = 0;
-
-  moveRight() {
-    if (this.count_clicks < 3) {
-      this.count_clicks++;
-      if (this.images[this.count_clicks]) {
-        this.current_image = this.images[this.count_clicks];
-      } else {
-      this.current_image = this.default_img;
-      }
-      if (this.employeeNames[this.count_clicks]) {
-        this.current_name = this.employeeNames[this.count_clicks];
-      } else {
-      this.current_name = 'Имя Фамилия';
-      }
-      if (this.employeeJobs[this.count_clicks]) {
-        this.current_job = this.employeeJobs[this.count_clicks];
-      } else {
-      this.current_job = 'Должность';
-      }
+  moveR() {
+    if (!this.move&&!this.move1&&!this.move2&&!this.move3) {
+      this.move = true;
+      this.move1 = false;
+      this.move2 = false;
+      this.move3 = false;
+    } else if (this.move&&!this.move1&&!this.move2&&!this.move3) {
+      this.move = false;
+      this.move1 = true;
+      this.move2 = false;
+      this.move3 = false;
+    } else if (!this.move&&this.move1&&!this.move2&&!this.move3) {
+      this.move = false;
+      this.move1 = false;
+      this.move2 = true;
+      this.move3 = false;
+    } else if (!this.move&&!this.move1&&this.move2&&!this.move3) {
+      this.move = false;
+      this.move1 = false;
+      this.move2 = false;
+      this.move3 = true;
+    } else if (!this.move&&!this.move1&&!this.move2&&this.move3) {
+      this.move = false;
+      this.move1 = false;
+      this.move2 = false;
+      this.move3 = false;
     }
   }
-
-  moveLeft() {
-    if (this.count_clicks > 0) {
-      this.count_clicks--;
-      this.current_image = this.images[this.count_clicks];
-      this.current_name = this.employeeNames[this.count_clicks];
-      this.current_job = this.employeeJobs[this.count_clicks];
+  moveL() {if (this.move&&!this.move1&&!this.move2&&!this.move3) {
+      this.move = false;
+      this.move1 = false;
+      this.move2 = false;
+      this.move3 = false;
+    } else if (!this.move&&this.move1&&!this.move2&&!this.move3) {
+      this.move = true;
+      this.move1 = false;
+      this.move2 = false;
+      this.move3 = false;
+    } else if (!this.move&&!this.move1&&this.move2&&!this.move3) {
+      this.move = false;
+      this.move1 = true;
+      this.move2 = false;
+      this.move3 = false;
+    } else if (!this.move&&!this.move1&&!this.move2&&this.move3) {
+      this.move = false;
+      this.move1 = false;
+      this.move2 = true;
+      this.move3 = false;
     }
-    // console.log(this.count_clicks,
-    //   this.current_image,
-    //   this.current_name,
-    //   this.current_job)
-    }
+  }
 
   arrowsShown = false;
   showArrows(){
